3da34f784fd0bd388088288de50c8c21ad2c2923
labyrinthofdreams <noemail@localhost.localhost>
2013-09-29 05:49:20 +0300

    initial commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+IMDb may fail to export very large rating lists. This script can be used to download
+ratings from the compact view.
+
+Note: The following fields are not available:
+
+created, modified, description, directors, runtime, genres, release date
---
+++ main.py
@@ -0,0 +1,118 @@
+import argparse
+import re
+import os.path
+import time
+import requests
+import bs4
+import csv, codecs, cStringIO
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+def get_start_positions(last_page):
+    cur_page = 1
+    while cur_page <= last_page:
+        start_pos = (cur_page - 1) * 250 + 1
+        yield (cur_page, start_pos)
+        cur_page += 1
+
+def pretty_seconds(t):
+    sep = lambda n: (int(n / 60), int(n % 60))
+    mins, secs = sep(t)
+    hrs, mins = sep(mins)
+    return '{0}h {1}m {2}s'.format(hrs, mins, secs)
+
+if __name__ == '__main__':
+    opts = argparse.ArgumentParser(description='Download large IMDb rating lists.')
+    opts.add_argument('ratings_url', help='URL to IMDb user ratings page')
+    opts.add_argument('outfile', help='Path to output CSV file')
+    args = opts.parse_args()
+
+    start_time = time.time()
+    # Get number of pages
+    print 'Retrieving number of pages'
+    url = args.ratings_url + '?start=1&view=compact&sort=ratings_date:desc&defaults=1'
+    resp = requests.get(url)
+    while resp.status_code != 200:
+        print 'Failed to retrieve number of pages (or private list)'
+        print 'Retrying...'
+        resp = requests.get(url)
+    parsed_html = bs4.BeautifulSoup(resp.text)
+    # Note: There should be only one element called div.desc
+    # but there's no guarantee
+    pages_text = parsed_html.find('div', class_='desc').get_text()
+    num_pages = int(re.search('Page 1 of ([0-9]+)', pages_text).group(1))
+
+    imdb = []
+    for cur_page, start_pos in get_start_positions(1):
+        print 'Downloading page', cur_page, 'of', num_pages
+        url = args.ratings_url + '?start=' + str(start_pos) + '&view=compact&sort=ratings_date:desc&defaults=1'
+        resp = requests.get(url)
+        while resp.status_code != 200:
+            print 'Failed to download page', cur_page, '(or private list)'
+            print 'Retrying...'
+            resp = requests.get(url)
+        html = bs4.BeautifulSoup(resp.text)
+        trs = html.find_all('tr', class_='list_item')
+        del trs[0] # Skip header
+        for tr in trs:
+            position = unicode(len(imdb) + 1)
+            html_title = tr.find('td', class_='title')
+            imdb_url = html_title.a['href']
+            imdb_id = re.search('(tt[0-9]{7})', imdb_url).group(1)
+            if 'episode' in html_title['class']:
+                # This ensures that there's a space between
+                # series title and episode name
+                html_title.a.append(' ')
+            title = html_title.get_text()
+            title_type = tr.find('td', class_='title_type').get_text().lstrip()
+            user_rating = tr.find('td', class_='rater_ratings').a.get_text()
+            imdb_rating = tr.find('td', class_='user_rating').get_text()
+            if imdb_rating == '0.0':
+                imdb_rating = u''
+            year = tr.find('td', class_='year').get_text()
+            num_votes = tr.find('td', class_='num_votes').get_text().replace(',', '')
+            if num_votes == '-':
+                num_votes = u'0'
+            url = unicode('http://www.imdb.com' + imdb_url)
+
+            imdb.append([position, imdb_id, u'', u'', u'', title, title_type,
+                         u'', user_rating, imdb_rating, u'', year, u'',
+                         num_votes, u'', url])
+    end_time = time.time()
+    print 'Downloaded', len(imdb), 'ratings in', pretty_seconds(end_time - start_time)
+    # Write results to CSV file
+    print 'Saving results to', args.outfile
+    username = os.path.splitext(os.path.basename(args.outfile))[0]
+    with codecs.open(args.outfile, 'wb') as outfile:
+        w = UnicodeWriter(outfile)
+        w.writerow(['position','const','created','modified','description','Title','Title type','Directors',
+                    '{0} rated'.format(username),'IMDb Rating','Runtime (mins)','Year','Genres','Num. Votes',
+                    'Release Date (month/day/year)','URL'])
+        w.writerows(imdb)
