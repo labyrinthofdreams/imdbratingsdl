e7e4643826c779d2ac7b7196426a48132e0f393f
labyrinthofdreams <noemail@localhost.localhost>
2015-09-07 11:54:04 +0300

    Remove append mode. Didn't really work with TV shows.

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ import csv
 import logging
 import os.path
 import re
+import sys
 import time
 import requests
 import bs4
@@ -83,7 +84,7 @@ def download_page(cur_page, start_pos):
     url = 'http://www.imdb.com/user/ur' + config['uid'] + '/ratings?start=' + str(start_pos) + '&view=compact&sort=ratings_date:desc&defaults=1'
     logger.info('Downloading page %s: %s', cur_page, url)
     trs = None
-    # Try downloading the page until we've received all 201 rows of rating data
+    # Try downloading the page until we've received all 251 rows of rating data
     while True:
         resp = session.get(url)
         if resp.status_code != 200:
@@ -97,16 +98,11 @@ def download_page(cur_page, start_pos):
         else:
             break
     print 'Parsing page', cur_page
-    abort = False
-    del trs[0] # Skip header
+    # Skip header
+    del trs[0]
     for tr in trs:
-        if abort:
-            break
         try:
-            if args.append:
-                position = unicode(int(imdb_old[-1][0]) + len(imdb_all) + 1)
-            else:
-                position = unicode(len(imdb_all) + 1)
+            position = unicode(len(imdb_all) + 1)
             html_title = tr.find('td', class_='title')
             imdb_url = html_title.a['href']
             imdb_id = re.search('(tt[0-9]{7})', imdb_url).group(1)
@@ -139,19 +135,10 @@ def download_page(cur_page, start_pos):
             if is_dupe and title_type not in ('TV Episode', 'TV Series'):
                 print 'Found a duplicate entry:', str(data)
                 logger.info('[%s] Found a duplicate entry: %s', args.outfile, str(data))
-            # Check if we reached an entry that already exists
-            found_last = any(e[1] == imdb_id for e in imdb_old)
-            last = None
-            if found_last:
-                last = [entry for entry in imdb_old if entry[1] == imdb_id][0]
-            if args.append and found_last and title_type not in ('TV Episode', 'TV Series') and user_rating == last[8]:
-                print 'Found the last new entry. Aborting...'
-                abort = True
-            else:
-                with codecs.open(args.outfile, 'ab') as outfile:
-                    w = UnicodeWriter(outfile)
-                    w.writerow(data)
-                imdb_all.append(data)
+            with codecs.open(args.outfile, 'ab') as outfile:
+                w = UnicodeWriter(outfile)
+                w.writerow(data)
+            imdb_all.append(data)
         except Exception as e:
             print 'Error: {0}'.format(str(e))
             logger.exception('Error while parsing: %s', str(e))
@@ -162,9 +149,13 @@ if __name__ == '__main__':
     opts.add_argument('outfile', help='Path to output CSV file')
     opts.add_argument('--start', type=int, default=1, help='Specify page number to start from')
     opts.add_argument('--cookies', default='cookies.txt', help='Load cookies from file')
-    opts.add_argument('--append', action='store_true', help='Append new entries and stop after found all new entries')
     opts.add_argument('--threads', default=3, type=int, help='Number of simultaneous downloads')
     args = opts.parse_args()
+
+    if os.path.exists(args.outfile):
+        answer = raw_input('Output file {0} already exists. Overwrite? [y/n]: '.format(args.outfile))
+        if answer.lower() == 'n':
+            sys.exit('Aborted')
 
     pool = Pool(args.threads)
 
@@ -201,20 +192,12 @@ if __name__ == '__main__':
         args.start = config['num_pages']
     imdb_all = []
     username = os.path.splitext(os.path.basename(args.outfile))[0]
-    outfile_exists = os.path.exists(args.outfile)
-    if not outfile_exists:
-        with codecs.open(args.outfile, 'wb') as outfile:
-            w = UnicodeWriter(outfile)
-            # Only output header if file didn't exist
-            w.writerow(['position','const','created','modified','description','Title','Title type','Directors',
-                        '{0} rated'.format(username),'IMDb Rating','Runtime (mins)','Year','Genres','Num. Votes',
-                        'Release Date (month/day/year)','URL'])
-    imdb_old = []
-    if args.append:
-        with codecs.open(args.outfile, 'rb') as infile:
-            r = csv.reader(infile)
-            for row in r:
-                imdb_old.append(row)
+    with codecs.open(args.outfile, 'wb') as outfile:
+        w = UnicodeWriter(outfile)
+        # Only output header if file didn't exist
+        w.writerow(['position','const','created','modified','description','Title','Title type','Directors',
+                    '{0} rated'.format(username),'IMDb Rating','Runtime (mins)','Year','Genres','Num. Votes',
+                    'Release Date (month/day/year)','URL'])
     for page in get_start_positions(config['num_pages'], args.start):
         pool.spawn(download_page, page[0], page[1])
     pool.join()
